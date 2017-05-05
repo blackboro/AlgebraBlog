dd1768676c19566db5249d026b060d08891ad915
programeri <programeri@ucione.local>
2017-05-05 18:32:58 +0200

    Dodan bootstrap viewovima

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,31 +1,22 @@
-<!DOCTYPE html>
-<html lang="hr">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>AlgebraBlog</title>
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+	                  @if ( count ($items) > 0 )
+						@foreach ($items as $item)
+					<h2><a href="items/{{ $item->id }} ">{{ $item->title }}</a> </h2>
+					
+					
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-     
-	  
-    </head>
-    <body>
-	
-		
-		@foreach ($items as $item)
-	<h2> 
-	<a href="items/{{ $item->id }} ">{{ $item->title }}</a> </h2>
-	
-	<div>{{ $item->content }} </div>
-	
-
-    @endforeach
-	
-       
-    </body>
-</html>
+					@endforeach
+					
+					@else
+						<h2> Nema zapisa u Blogu! </h2>
+						@endif
+			   
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,17 @@
-<!DOCTYPE html>
-<html lang="hr">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>LARAVEL PROJEKT</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+@extends('layouts.app')
 
-        <!-- Styles -->
-     
-	  
-    </head>
-    <body>
-	
-	
-	<h2><a href="items">Dobrodošli na AlgebraBlog!</a></h2>
-    
-	
-       
-    </body>
-</html>
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+         
+                <h2><a href="items">Dobrodošli na AlgebraBlog!</a></h2>
+			   
+        </div>
+    </div>
+</div>
+@endsection
+
+
